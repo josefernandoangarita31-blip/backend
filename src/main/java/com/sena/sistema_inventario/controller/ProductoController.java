9893552dcc911de0843a5514cc3a6a3b46cd720e
josefernandoangarita31-blip <josefernando.angarita.31@gmail.com>
2026-09-19 09:43:49 -0500

    o

--- src/main/java/com/sena/sistema_inventario/controller/ProductoController.java
+++ src/main/java/com/sena/sistema_inventario/controller/ProductoController.java
@@ -10,12 +10,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sena.sistema_inventario.model.Producto; 
 import com.sena.sistema_inventario.service.ProductoService;
 
-@CrossOrigin(origins = "*")
-@RestController 
+@RestController
+@RequestMapping("/api/productos")
+@CrossOrigin(origins = "https://inventariojosue.netlify.app")
+
 public class ProductoController { 
 
     private final ProductoService service; 
